c65617a2b69f143639e60698cd655fcc3e692011
Ashiq <shiqtec@gmail.com>
2022-06-05 15:42:26 +0200

    Add logging with Serilog

--- QuartzNET/QuartzWorkerService/Program.cs
+++ QuartzNET/QuartzWorkerService/Program.cs
@@ -1,20 +1,28 @@
 using Quartz;
 using QuartzWorkerService;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.File("logs/log.txt",
+        restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information,
+        rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 
 IHost host = Host.CreateDefaultBuilder(args)
+    .UseSerilog()
     .ConfigureServices((hostBuilder, services) =>
     {
         services.AddQuartz(quartzConfig =>
         {
             quartzConfig.UseMicrosoftDependencyInjectionJobFactory();
 
-            var jobKey = new JobKey("HelloWorldJob");
+            var jobKey = new JobKey("MyJob");
 
             quartzConfig.AddJob<MyJob>(options => options.WithIdentity(jobKey));
             quartzConfig.AddTrigger(options => options
                 .ForJob(jobKey)
-                .WithIdentity("HelloWorldJob-trigger")
-                .WithCronSchedule(hostBuilder.Configuration["Quartz:HelloWorldJob"]));
+                .WithIdentity("MyJob-trigger")
+                .WithCronSchedule(hostBuilder.Configuration["Quartz:MyJob"]));
         });
 
         services.AddQuartzHostedService(
