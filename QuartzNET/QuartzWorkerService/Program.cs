df3a05e80ecc7defa7bae7bf394beb295228ca6e
Ashiq <shiqtec@gmail.com>
2022-06-05 14:23:06 +0200

    Add initial with Quartz Hosting package

---
+++ QuartzNET/QuartzWorkerService/Program.cs
@@ -0,0 +1,10 @@
+using QuartzWorkerService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
